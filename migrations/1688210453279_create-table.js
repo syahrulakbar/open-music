1bfd1c14bc0640a66b78b8bec1caa48d4b7e0a8a
Mochamad Syahrul Akbar <07tav2akbar@gmail.com>
2023-07-01 23:39:07 +0700

    feat(migrations): add table songs and albums

---
+++ migrations/1688210453279_create-table.js
@@ -0,0 +1,53 @@
+/* eslint-disable camelcase */
+
+exports.up = (pgm) => {
+  pgm.createTable("albums", {
+    id: {
+      type: "VARCHAR(50)",
+      primaryKey: true,
+    },
+    name: {
+      type: "VARCHAR(255)",
+      notNull: true,
+    },
+    year: {
+      type: "integer",
+      notNull: true,
+    },
+  });
+  pgm.createTable("songs", {
+    id: {
+      type: "VARCHAR(50)",
+      primaryKey: true,
+    },
+    title: {
+      type: "VARCHAR(255)",
+      notNull: true,
+    },
+    year: {
+      type: "integer",
+      notNull: true,
+    },
+    genre: {
+      type: "VARCHAR(255)",
+      notNull: true,
+    },
+    performer: {
+      type: "VARCHAR(255)",
+      notNull: true,
+    },
+    duration: {
+      type: "integer",
+    },
+    albumId: {
+      type: "VARCHAR(50)",
+      references: "'albums'",
+      onDelete: "cascade",
+    },
+  });
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable("albums");
+  pgm.dropTable("songs");
+};
